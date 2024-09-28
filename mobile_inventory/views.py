4b7b682e9bff54a434c5b18702a43b30717abcca
realsoli <realsolii778@gmail.com>
2024-09-28 16:05:10 +0330

    update forms

--- mobile_inventory/views.py
+++ mobile_inventory/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.db.models import Q
+from django.shortcuts import redirect
 from django.views import View
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.http import JsonResponse
 from .models import Brand, Phone, Country, Color
@@ -26,35 +27,97 @@ class PhoneListView(ListView):
         return queryset
 
 
-class AddBrandView(CreateView):
-    model = Brand
-    form_class = BrandForm
-    template_name = 'mobile_inventory/add_brand.html'
-    success_url = reverse_lazy('list_phone')  # آدرس صفحه بعد از ذخیره موفق
-
-
-class AddCountryView(CreateView):
-    model = Country
-    from_class = CountryForm
-    template_name = 'mobile_inventory/add_country.html'
-    success_url = reverse_lazy('add_phone')
-
-
-class AddColorView(CreateView):
-    model = Color
-    from_class = ColorForm
-    template_name = 'mobile_inventory/add_color.html'
-    success_url = reverse_lazy('add_phone')
-
-
 class AddPhoneView(CreateView):
     model = Phone
     form_class = PhoneForm
     template_name = 'mobile_inventory/add_phone.html'
     success_url = reverse_lazy('list_phone')  # آدرس صفحه بعد از ذخیره موفق
 
+    def dispatch(self, *args, **kwargs):
+        # بررسی اینکه حداقل یک برند، رنگ و کشور در دیتابیس وجود داشته باشد
+        if not Brand.objects.exists() or not Color.objects.exists() or not Country.objects.exists():
+            return redirect('add_brand')  # اگر یکی از آن‌ها وجود نداشت، به صفحه افزودن برند بازگردانده شود
+        return super().dispatch(*args, **kwargs)
 
-class PhonesByBrandView(View):
-    def get(self, request, brand_name):
-        phones = Phone.objects.filter(brand__name=brand_name).values('model', 'price')
-        return JsonResponse(list(phones), safe=False)
+
+# -------------------------------------------------------------------
+
+class AddBrandView(CreateView):
+    model = Brand
+    form_class = BrandForm
+    template_name = 'mobile_inventory/add_brand.html'
+    success_url = reverse_lazy('add_color')  # آدرس صفحه بعد از ذخیره موفق
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['brands'] = Brand.objects.all()  # اضافه کردن لیست برند ها به context
+        return context
+
+
+class EditBrandView(UpdateView):
+    model = Brand
+    form_class = BrandForm
+    template_name = 'mobile_inventory/edit_brand.html'
+    success_url = reverse_lazy('add_brand')  # آدرس برگشت بعد از ویرایش
+
+
+class DeleteBrandView(DeleteView):
+    model = Brand
+    template_name = 'mobile_inventory/delete_brand.html'
+    success_url = reverse_lazy('add_brand')  # آدرس برگشت بعد از ویرایش
+
+
+# ---------------------------------------------------------------
+
+
+class AddColorView(CreateView):
+    model = Color
+    form_class = ColorForm
+    template_name = 'mobile_inventory/add_color.html'
+    success_url = reverse_lazy('add_country')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['colors'] = Color.objects.all()  # اضافه کردن لیست رنگ‌ها به context
+        return context
+
+
+class EditColorView(UpdateView):
+    model = Color
+    form_class = ColorForm
+    template_name = 'mobile_inventory/edit_color.html'
+    success_url = reverse_lazy('add_color')  # آدرس برگشت بعد از ویرایش
+
+
+class DeleteColorView(DeleteView):
+    model = Color
+    template_name = 'mobile_inventory/delete_color.html'
+    success_url = reverse_lazy('add_color')
+
+
+# -------------------------------------------------------------------
+
+
+class AddCountryView(CreateView):
+    model = Country
+    form_class = CountryForm
+    template_name = 'mobile_inventory/add_country.html'
+    success_url = reverse_lazy('add_phone')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['countries'] = Country.objects.all()
+        return context
+
+
+class EditCountryView(UpdateView):
+    model = Country
+    form_class = CountryForm
+    template_name = 'mobile_inventory/edit_country.html'
+    success_url = reverse_lazy('add_country')
+
+
+class DeleteCountryView(DeleteView):
+    model = Country
+    template_name = 'mobile_inventory/delete_country.html'
+    success_url = reverse_lazy('add_country')
